471d830497e2bb8563f82250b5c1c4d9c6da637b
Kanore <derro91@gmail.com>
2023-10-03 10:29:18 -0700

    Config

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Create a file using puppet
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
+# Create file in /tmp
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
